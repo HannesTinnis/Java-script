9241a1ab3ec0ad060a231ec198f1735e715428b6
HannesTinnis <hannes.tinnis@elev.ga.ntig.se>
2023-12-12 10:32:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("hej")
+const btnHello = document.getElementById("demmo")
+const btn = document.getElementById("button")
+function myFunctionn() {
+    btnHello.innerHTML = "Hej Hannes";
+    btn.classList.toggle("newbackground")
+  }
+function myFunction() {
+alert("Hej Hej");
+}
